116fb266ac5e6cb261dfa5a2fb0aa8ac9ab9d380
rashan213 <103337865+rashan213@users.noreply.github.com>
2022-04-18 21:21:58 +0800

    main

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x1EF2DFaEf0D8A6283D6fbbd534258051B3BA4F26"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
