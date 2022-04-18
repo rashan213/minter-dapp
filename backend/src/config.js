2f41d12fc4de9b73f399a544f91cdd17daf0bd08
rashan213 <103337865+rashan213@users.noreply.github.com>
2022-04-18 21:48:00 +0800

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. 
 const TOKENS_PER_MINT = 0.001; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-10T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-11T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-04-15T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
